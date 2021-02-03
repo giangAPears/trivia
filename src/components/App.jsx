e58cffe1c607e5db6d655af89907e28c73914bac
Th30s <43751800+Th30s@users.noreply.github.com>
2021-02-03 18:27:01 -0500

    nearly completed goal 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,55 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let questionNum = 0;
+
+  let [isAnswered, setisAnswered] = useState(false);
+
+  function showAnswer() {
+    setisAnswered(true);
+  }
+
+  let [answerSentence, setanswerSentence] = useState("unanswered");
+  /*
+{
+isAnswered && <Component/>
+}
+*/
+  if (isAnswered === true) {
+    setanswerSentence(data[0].question.choices[3]);
+  }
+  return (
+    <div className="app">
+      {" "}
+      <h1> Trivia </h1>
+      <Question
+        question={data[0].question.text}
+        choices={data[0].question.choices}
+      />
+      <NextQuestion renderBtn="Next Question" />
+      <button onClick={showAnswer}>Show Answer</button>
+      <p>The correct answer is {answerSentence}</p>
+    </div>
+  );
+}
+
+function Question(props) {
+  return (
+    <div className="questionPadding">
+      {props.question}
+      {props.choices.map((x) => {
+        return <Answer answer={x} />;
+      })}
+    </div>
+  );
+}
+
+function NextQuestion(props) {
+  return <button> {props.renderBtn} </button>;
+}
+
+function Answer(props) {
+  return <p> {props.answer}</p>;
 }
 
 export default App;
