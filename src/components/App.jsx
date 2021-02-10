b32c5b55535acc470f993bbff30d6a71e5bdee62
Th30s <43751800+Th30s@users.noreply.github.com>
2021-02-10 18:24:54 -0500

    Completed Day 3 Goal 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,33 +3,46 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  let questionNum = 0;
 
   let [isAnswered, setisAnswered] = useState(false);
-
+  let [questionNum, setquestionNum] = useState(0);
   function showAnswer() {
     setisAnswered(true);
+    if (isAnswered === true) {
+      setanswerSentence(data[questionNum].question.choices[data[questionNum].question.correct_choice_index]);
+    }
   }
-
+  
+  let nextQuestionNum=() => 
+  {setquestionNum(questionNum + 1)
+  setanswerSentence("unanswered")
+  setisAnswered(false)    
+  }
   let [answerSentence, setanswerSentence] = useState("unanswered");
   /*
-{
-isAnswered && <Component/>
-}
+
 */
-  if (isAnswered === true) {
-    setanswerSentence(data[0].question.choices[3]);
-  }
   return (
     <div className="app">
       {" "}
       <h1> Trivia </h1>
       <Question
-        question={data[0].question.text}
-        choices={data[0].question.choices}
+        question={data[questionNum].question.text}
+        choices={data[questionNum].question.choices}
       />
-      <NextQuestion renderBtn="Next Question" />
-      <button onClick={showAnswer}>Show Answer</button>
+      <NextQuestion  renderBtn="Next Question" 
+      questionNum = {questionNum}
+      setquestionNum = {setquestionNum}
+      nextQuestionNum = {nextQuestionNum}
+      />
+      {
+        //questionNum will only be 0 or 1, data.length will always be 2
+        //when questionNum is 1, we want the expression below to be false
+        //+ be sure to commit!
+    //questionNum < data.length  && <Component/>
+
+}
+      <button  onClick={showAnswer}>Show Answer</button>
       <p>The correct answer is {answerSentence}</p>
     </div>
   );
@@ -47,7 +60,7 @@ function Question(props) {
 }
 
 function NextQuestion(props) {
-  return <button> {props.renderBtn} </button>;
+  return <button onClick = {props.nextQuestionNum} > {props.renderBtn} </button>;
 }
 
 function Answer(props) {
