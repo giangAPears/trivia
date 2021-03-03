38ac1c3a938e9dada95c7a35e28ba799d6b0a9b0
Th30s <43751800+Th30s@users.noreply.github.com>
2021-03-03 18:25:26 -0500

    Made the answers into buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,29 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-
   let [isAnswered, setisAnswered] = useState(false);
   let [questionNum, setquestionNum] = useState(0);
   function showAnswer() {
     setisAnswered(true);
-    if (isAnswered === true) {
-      setanswerSentence(data[questionNum].question.choices[data[questionNum].question.correct_choice_index]);
-    }
+    setanswerSentence(
+      data[questionNum].question.choices[
+        data[questionNum].question.correct_choice_index
+      ]
+    );
   }
-  
-  let nextQuestionNum=() => 
-  {setquestionNum(questionNum + 1)
-  setanswerSentence("unanswered")
-  setisAnswered(false)    
-  }
-  let [answerSentence, setanswerSentence] = useState("unanswered");
-  /*
 
-*/
+  let nextQuestionNum = () => {
+    setquestionNum(questionNum + 1);
+    setanswerSentence("unanswered");
+    setisAnswered(false);
+  };
+  let [answerSentence, setanswerSentence] = useState("unanswered");
+
+  function handleChoice(event) {
+    var valChoice = event.target.value
+    console.log(valChoice);
+  };
+
   return (
     <div className="app">
       {" "}
@@ -29,42 +33,40 @@ function App() {
       <Question
         question={data[questionNum].question.text}
         choices={data[questionNum].question.choices}
+        handleChoice = {handleChoice}
       />
-      <NextQuestion  renderBtn="Next Question" 
-      questionNum = {questionNum}
-      setquestionNum = {setquestionNum}
-      nextQuestionNum = {nextQuestionNum}
-      />
-      {
-        //questionNum will only be 0 or 1, data.length will always be 2
-        //when questionNum is 1, we want the expression below to be false
-        //+ be sure to commit!
-    //questionNum < data.length  && <Component/>
-
-}
-      <button  onClick={showAnswer}>Show Answer</button>
+      {questionNum < data.length - 1 && (
+        <NextQuestion
+          renderBtn="Next Question"
+          questionNum={questionNum}
+          setquestionNum={setquestionNum}
+          nextQuestionNum={nextQuestionNum}
+        />
+      )}
+      <button onClick={showAnswer}>Show Answer</button>
       <p>The correct answer is {answerSentence}</p>
     </div>
   );
 }
+
 
 function Question(props) {
   return (
     <div className="questionPadding">
       {props.question}
       {props.choices.map((x) => {
-        return <Answer answer={x} />;
+        return <Answer handleChoice = {props.handleChoice}  answer={x} />;
       })}
     </div>
   );
 }
 
 function NextQuestion(props) {
-  return <button onClick = {props.nextQuestionNum} > {props.renderBtn} </button>;
+  return <button onClick={props.nextQuestionNum}> {props.renderBtn} </button>;
 }
 
 function Answer(props) {
-  return <p> {props.answer}</p>;
+return <button onClick= {props.handleChoice} value={props.answer}> {props.answer} </button>;
 }
 
 export default App;
